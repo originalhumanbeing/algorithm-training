09f13e85e52d3ebc587f47a3961dc2ebe8f33a2f
hyunjung.han <han.hyunjung17@gmail.com>
2017-09-19 23:46:15 +0900

    practice comparing string and using stack to solve problem with java

---
+++ src/goorm/MazeGame.java
@@ -0,0 +1,24 @@
+package goorm;
+
+import java.util.List;
+
+/**
+ * Created by Hyunjung on 2017. 9. 19..
+ */
+public class MazeGame {
+    public static void main(String[] args) {
+
+    }
+
+
+    private static boolean go(List<List<Integer>> map){
+        int i = 0 , j = 0;
+        while(true) {
+            int value = map.get(i).get(j);
+            break;
+        }
+
+        return true;
+    }
+
+}
